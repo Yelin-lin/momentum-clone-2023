302709afc250eec9eeb37840a914459a7858d2c0
Yelin-lin <77089775+Yelin-lin@users.noreply.github.com>
2023-03-03 15:13:47 +0900

    Update : backgroundimage 추가 및 시간별로 변경되도록 하기

--- js/background.js
+++ js/background.js
@@ -1,10 +1,20 @@
-const images = ["0.jpg","1.jpg","2.jpg"];
+const imagesList = ["0.jpg","1.jpg","2.jpg", "3.jpg"];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)]
+const chosenImage = imagesList[Math.floor(Math.random() * imagesList.length)]
 
 const bgImage = document.createElement("img");
 
 bgImage.className = "bgImageName";
-bgImage.src = `img/${chosenImage}`;
+
+const bgDate = new Date();
+const bgHours = bgDate.getHours();
+
+if(bgHours < 12){
+    bgImage.src = `img/morning/${chosenImage}`;
+} else if(bgHours == 12){
+    bgImage.src = `img/at noon/${chosenImage}`;
+} else if(bgHours > 12){
+    bgImage.src = `img/afternoon/${chosenImage}`;
+}
 
 document.body.appendChild(bgImage);
